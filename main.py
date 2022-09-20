c73aa200c0704696400b8bf096d24ee137d021ca
Adi P <aprakash1353@gmail.com>
2022-09-20 17:43:50 +0000

    20 Sept 2022, 18:43

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ while True:
     ui_surface = font.render(user_input,True,(0, 0, 0))
     screen.blit(ui_surface, (text_container.x + 5, text_container.y + 10))
     text_container.w = max(100, ui_surface.get_width() + 10)
-    text_container.h = max(50, ui_surface.get_height())
+    text_container.h = max(50, ui_surface.get_height() + 50)
     
     
     

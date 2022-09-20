70dd536827c311c0dd8bf31e18f81164e1f7fbff
Adi P <aprakash1353@gmail.com>
2022-09-20 18:24:35 +0100

    Created basic window and text input

--- functions.py
+++ functions.py
@@ -1,67 +1,9 @@
-import sys
-import time
-import os
+import pygame
+from main import *
 
-
-def write(s):
-    for i in s:
-        print(i, end="", flush=True)
-        time.sleep(.075)
-
-
-def beginning():
-
-    write('''
-  Hello Adventurer !
-  I am your guide , Svaltek,
-  what should I call you ?
-  ''')
-
-    global username
-    username = input("Enter name: ")
-
-    write(f'''
-  Warm greetings {username}!
-  You don't remember anything, do you ?
-  ''')
-
-    prompt1 = input(">")
-    print(prompt1.lower)
-
-    if prompt1.lower == "yes":
-      write('''
-      *Svaltek mutters anxiously, but you hear him*
-      
-      Maybe I should re-do the ritual,
-      he's too dangerous like this
-      ''')
-    elif prompt1.lower == "no":
-      write(f'''
-      *Svaltek breathes a sigh of relief*
-    
-      Well then {username}, we should get you some food!
-      What do you fancy , some soup or chicken ?
-      ''')
-      soup_or_chicken = input(">")
-    if soup_or_chicken.lower != "soup" or "chicken":
-      write('''
-        *Svaltek shouts angrily*
-                  
-          ANSWER ME ya skinny pillock or I'll CLOBBER ya
-          over your thick skull!
-            ''')
-    else:
-      write('''
-        *Svaltek grins toothily*
-            
-        Well jokes on you , we've got neither,
-        I've some dried reindeer in my pack,
-        you can have a roll.
-          ''')
-    elif prompt1.lower != "yes" or "no":
-      write("Do you remember? YES or NO")
-  
-  
-
-
-
+def drawtextinput():
+    pygame.draw.rect(screen,text_container_color,text_container,2,3)
+    ui_surface = font.render(user_input,True,(0, 0, 0))
+    screen.blit(ui_surface, (text_container.x + 5, text_container.y + 10))
+    text_container.w = max(100, ui_surface.get_width() + 10)
+    text_container.h = max(50, ui_surface.get_height())
--- main.py
+++ main.py
@@ -1,5 +1,71 @@
-from functions import *
-import sys
-import time
+import pygame
+from sys import exit
+from ctypes import windll
+from pygame.locals import * 
 
-beginning()
+
+windll.shcore.SetProcessDpiAwareness(1)
+
+w, h = [1200, 800]
+
+pygame.init()
+
+# Name and icon
+screen = pygame.display.set_mode((w, h), RESIZABLE)
+pygame.display.set_caption('The Shilton Diaries')
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+clock = pygame.time.Clock()
+
+# Surface for player input
+player_sur = pygame.Surface((w, h/3))
+player_sur.fill((11, 252, 3))
+
+# Surface for the NPC interactions
+NPC_surface = pygame.Surface((w, h*2/3))
+NPC_surface.fill((252, 3, 3))
+
+# Player text box
+font = pygame.font.Font("fonts/Peepo.ttf", 30)
+user_input = ""
+text_container = pygame.Rect(w/2,300, 140, 32)
+text_container_color = pygame.Color('red')
+
+
+ # Game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            
+            if event.key == pygame.K_BACKSPACE:
+                pygame.key.set_repeat(200, 50) 
+                user_input = user_input[:-1]               
+            elif event.key == pygame.K_RETURN:
+                pass
+            else: 
+                user_input += event.unicode
+                      
+    # Draws user input and NPC output boxes
+    screen.blit(player_sur,(0, h-h/3))
+    screen.blit(NPC_surface, (0,0))
+
+    # Draws user text box 
+    pygame.draw.rect(screen,text_container_color,text_container,2,3)
+    ui_surface = font.render(user_input,True,(0, 0, 0))
+    screen.blit(ui_surface, (text_container.x + 5, text_container.y + 10))
+    text_container.w = max(100, ui_surface.get_width() + 10)
+    text_container.h = max(50, ui_surface.get_height())
+    
+    
+    
+    
+    
+    
+    
+            
+            
+    pygame.display.update()
+    clock.tick(60)
